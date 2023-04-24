276941cc51d46d793576793bd094651d4f8a770a
LC-02s <cl700120@gmail.com>
2023-04-24 23:55:45 +0900

    start project

---
+++ app.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+
+// 라우팅
+const home = require('./routes/home');
+
+// 앱 세팅
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use('/', home); // use -> 미들 웨어를 등록해주는 메서드.
+
+module.exports = app;
+
+// const http = require('http');
+// const app = http.createServer((req, res) => {
+//     console.log(req.url);
+//     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+//     if (req.url === '/') {
+//         res.end("루트 경로");
+//     } else if (req.url === '/login') {
+//         res.end("로그인 경로");
+//     };
+// });
+// const PORT = 3001;
+
+// app.listen(PORT, () => {
+//     console.log(`서버 실행 : ${PORT}`);
+// });
