0a9cb7a69ae3e905cb5567aab325843e05a81fe2
LC-02s <cl700120@gmail.com>
2023-04-30 21:28:23 +0900

    add DOM HTML element

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 "use strict"
 
-console.log("hello");
-console.log("123");
+const idEl = document.getElementById("id"),
+    pwEl = document.getElementById("pw"),
+    loginBtnEl = document.getElementById("loginBtn");
+
+loginBtnEl.addEventListener('click', login);
+
+function login() {
+    const req = {
+        id: idEl.value,
+        pw: pwEl.value,
+    };
+    console.log(`id : ${req.id}, pw : ${req.pw}`);
+};
