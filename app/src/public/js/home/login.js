6addb750f8aacfcedac2700816db10880eeeb90e
LC-02s <cl700120@gmail.com>
2023-04-30 22:07:19 +0900

    create login fetch

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -12,4 +12,13 @@ function login() {
         pw: pwEl.value,
     };
     console.log(`id : ${req.id}, pw : ${req.pw}`);
+    // console.log(JSON.stringify(req));
+
+    fetch('/login', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    });
 };
