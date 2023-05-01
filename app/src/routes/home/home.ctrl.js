856f83569f741779e26f341c8084749575e3579f
LC-02s <cl700120@gmail.com>
2023-05-01 21:02:42 +0900

    add user model

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const UserStorage = require('../../models/UserStorage');
+// const UserStorage = require('../../models/UserStorage');
+const User = require('../../models/User');
 
 const output = {
     home: (req, res) => {
@@ -13,23 +14,28 @@ const output = {
 
 const process = {
     login: (req, res) => {
-        const id = req.body.id,
-            pw = req.body.pw,
-            response = {};
-        
-        const users = UserStorage.getUsers("id", "pw");
+        const user = new User(req.body);
+        const reponse = user.login();
+        // console.log(reponse);
+        return res.json(reponse);
 
-        if (users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if (users.pw[idx] === pw) {
-                response.success = true;
-                return res.json(response);
-            } else {
-                response.success = false;
-                response.msg = "로그인에 실패하셨습니다.";
-                return res.json(response);
-            };
-        };
+        // const id = req.body.id,
+        //     pw = req.body.pw,
+        //     response = {};
+        
+        // const users = UserStorage.getUsers("id", "pw");
+
+        // if (users.id.includes(id)) {
+        //     const idx = users.id.indexOf(id);
+        //     if (users.pw[idx] === pw) {
+        //         response.success = true;
+        //         return res.json(response);
+        //     };
+        // };
+
+        // response.success = false;
+        // response.msg = "로그인에 실패하셨습니다.";
+        // return res.json(response);
     },
 };
 
